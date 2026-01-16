69dbd0a38d71e989e46c434e2cdf5a90b8e8146a
Shimpi Rajawat <shimpirajawat360@gmail.com>
2026-01-16 20:54:50 +0530

    updated backend

--- backend/venv/bin/activate.fish
+++ backend/venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/shimpirajawat/Desktop/video streaming project/rtsp-frontend/backend/venv"
+set -gx VIRTUAL_ENV "/Users/shimpirajawat/Desktop/video streaming project/backend/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
